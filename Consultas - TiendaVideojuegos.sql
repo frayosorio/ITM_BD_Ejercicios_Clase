7135b7fa35793dd4f1398b47f62539b91a1dab92
fray osorio <frayosorio@gmail.com>
2025-09-05 19:41:01 -0500

    Se agregan nuevas instrucciones INSERT para poblar la BD de Tienda de Videojuegos

--- Consultas - TiendaVideojuegos.sql
+++ Consultas - TiendaVideojuegos.sql
@@ -1,2 +1,15 @@
 SELECT *
 	FROM EstadoVenta
+
+SELECT * FROM TipoDocumento
+
+SELECT * FROM Pais
+	WHERE Id=170
+
+SELECT COUNT(*) FROM Pais
+
+SELECT *
+	FROM Region
+		JOIN Pais ON Region.IdPais=Pais.Id
+
+SELECT COUNT(*) FROM Ciudad
